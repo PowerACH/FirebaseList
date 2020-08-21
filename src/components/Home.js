170464533322bdda7c3774de687aba555c9b49ae
Rachel Powe <rachelpowe@gmail.com>
2020-08-21 12:57:51 -0400

    added state content

---
+++ src/components/Home.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import Todo from './Todo'
+import List from './List'
+
+
+export default class Home extends React.Component {
+
+    state = {
+        list: [
+            {id:1, entry: "Go to the grocery store"},
+            {id:2, entry: "Become a master at React"}
+        ]
+    }
+
+    addAnEntry = list => {
+        list.id = Math.random()*100000000000;
+        let entry = [...this.state.list, list]
+        this.setState({
+            list: entry
+        })
+    }
+
+    render() {
+        return(
+            <div className = "container">
+                <Todo addAnEntry = { this.addAnEntry } />
+                <List list={ this.state.goals } />
+            </div>
+        )
+    }
+}
