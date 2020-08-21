4dfc80658d7aeeb589661683ace699c3a2407dc5
Rachel Powe <rachelpowe@gmail.com>
2020-08-21 12:48:45 -0400

    added navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <nav>
+                    <div className="nav-wrapper">
+                        <div className = "container">
+                            <a className = "brand-logo center">List</a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        )
+    }
+}
