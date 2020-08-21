4905f773b92e13f8b173d270f61b413215af32d6
Rachel Powe <rachelpowe@gmail.com>
2020-08-21 14:17:16 -0400

    added content

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,2 +1,37 @@
 import React from 'react';
 
+class Todo extends React.Component {
+    state = {
+        entry: ''
+    }
+
+    getEntry = e => {
+        e.preventDefault()
+        if(this.state.entry !== ''){
+            this.props.addAnEntry(this.state)
+            this.setState({ entry: '' })
+        }
+    }
+
+    render() {
+        return(
+            <div>
+                <div className = "row">
+                    <form className="col s12" style={{ marginTop: '70px '}} 
+                    onSubmit={this.getEntry }>
+                        <input type="text"
+                        onChange={ e=> {this.setState({ entry: e.target.value })}
+                        }
+                        value={ this.state.entry }
+                        require />
+                        <div className='center'>
+                            <button className='btn btn-large blue' 
+                                onClick={this.getEntry }> Add Entry 
+                            </button>
+                        </div>
+                    </form>
+                </div>
+        </div>
+        )
+    }
+}
