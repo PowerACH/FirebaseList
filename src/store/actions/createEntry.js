c11600fe5e35285223dfc442bcb25e8860755569
Rachel Powe <rachelpowe@gmail.com>
2020-08-23 14:04:01 -0400

    notes

--- src/store/actions/createEntry.js
+++ src/store/actions/createEntry.js
@@ -1,3 +1,5 @@
+// This function is fired when we want to add an entry to the store or when we dispatch an action for adding an entry to the store.
+
 export const createEntryAction = (entry) => {
     entry.id = Math.random()*100000000000;
 
