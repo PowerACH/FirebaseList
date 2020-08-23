70e355640c41e4cbd8fba0866f0e852cba886ad0
Rachel Powe <rachelpowe@gmail.com>
2020-08-23 13:54:50 -0400

    redux imports

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux'
+// This component ^^ returns a function that takes in the component (Todo) which we want to connect to the store as an argument
+import { createEntryAction } from '../store/actions/createEntry'
 
-export default class Todo extends React.Component {
+class Todo extends React.Component {
     state = {
         entry: ''
     }
 
     getEntry = e => {
         e.preventDefault()
+        
         if(this.state.entry !== ''){
             this.props.addAnEntry(this.state)
             this.setState({ entry: '' })
+            console.log(this.state.entry)
         }
     }
 
@@ -35,3 +40,16 @@ export default class Todo extends React.Component {
         )
     }
 }
+
+
+// This function returns an object, the object is then automatically attached to the props or properties of our component.
+// The “mapDispatchToProps” function, on the other hand, is mainly used to dispatch an action which is to mutate the state or the data in the store. This function is also automatically attached to the props of the connected component.
+const mapDispatchToProps = (dispatch) => {
+    return {
+        createEntry: (entry) => { dispatch(createEntryAction(entry))}
+    }
+}
+
+export default connect(null, mapDispatchToProps)(Todo);
+// The connect component actually takes in two arguments, one is the “mapStateToProps” function which enables us to have access to the data in the store. Hence, data is easily accessed from the store via this function which is then automatically attached to the props of the component.
+// 
