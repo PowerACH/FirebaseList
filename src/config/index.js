575a4f6f6fbaf29cb9bf25c6869183bcebeb7428
Rachel Powe <rachelpowe@gmail.com>
2020-08-23 13:55:03 -0400

    added config for firebase

---
+++ src/config/index.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAdEs7fWmAf6BHqN8AZns22fScHue_Q3SM",
+    authDomain: "listme-7710c.firebaseapp.com",
+    databaseURL: "https://listme-7710c.firebaseio.com",
+    projectId: "listme-7710c",
+    storageBucket: "listme-7710c.appspot.com",
+    messagingSenderId: "368439906988",
+    appId: "1:368439906988:web:ee98e3606594617815c509",
+    measurementId: "G-CFQ3T773DX"
+  };
