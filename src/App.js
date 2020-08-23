f3a5f013535c7a6d8714ea5e17446ea5946db113
Rachel Powe <rachelpowe@gmail.com>
2020-08-23 17:07:22 -0400

    added path

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 
+import singleEntry from './components/AnEntry'
 
 
 export default class App extends Component {
@@ -15,6 +16,7 @@ export default class App extends Component {
          <Navbar />
          <Switch>
           <Route path='/' component={ Home } />
+          <Route path='/:id' component={ singleEntry } />
          </Switch>
        </div>
       </BrowserRouter>
