4c839a33adfca7eaff266e932cf61273d12c6eba
Rachel Powe <rachelpowe@gmail.com>
2020-08-23 13:54:32 -0400

    redux imports

--- src/index.js
+++ src/index.js
@@ -4,9 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// these steps help us create a store and also connect our application to the store. i.e we create a store with the createStore function from redux, and then, connect the store to our application with the Provider component which takes the store we have created as a props.
+
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './store/reducers/rootReducer';
+
+const Store = createStore(rootReducer);
+
 ReactDOM.render(
   <React.StrictMode>
+  <Provider store={ Store }>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
